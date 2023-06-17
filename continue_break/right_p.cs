5b833e293e758350eb72662d852402e5528c366d
anjali buddhe <anjalibuddhe124@gmail.com>
2023-06-17 19:09:39 +0530

    Add project files.

---
+++ continue_break/right_p.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace continue_break
+{
+    internal class right_p
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=5;i++)
+            {
+                for (int j =i - 1; j>=i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i ; k++)
+                {
+                    Console.Write(" *");
+                }
+                
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
